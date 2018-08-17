08edce96b2d1fdfd21c42a9642e205731ad60d8d
Loser Teach <jetaimefrc@gmail.com>
2018-08-17 14:21:07 +0700

    Complete Node Basic

--- FirstProject/app.js
+++ FirstProject/app.js
@@ -1,14 +1,19 @@
 const notes = require('./notes.js');
+const yarsX = require('./node_modules/yargs');
 const _ = require('./node_modules/lodash');
-const yargs = require('yargs');
-const js = require('fs');
+const argv = yarsX.arguments
+    .command('add', 'Add a note', {
+        title: {
+            describe: 'Tilte of note',
+            demand: true,
+            alias: 't'
+        }
+    })
+    .help()
+    .argv;
 
+const command = argv._[0];
 
-const argv = yargs.argv;
-const command = process.argv[2];
-console.log('Command', command);
-console.log('Process', process.argv);
-console.log('Yargs', argv);
 if (command === 'add') {
     const note = notes.addNote(argv.title, argv.body);
     if (note) {
@@ -19,9 +24,9 @@ if (command === 'add') {
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
-    notes.getNote(argv.title);
+    notes.getNote(command.title);
 } else if (command === 'remove') {
-    const noteRemove = notes.removeNote(argv.title);
+    const noteRemove = notes.removeNote(command.title);
     const message = noteRemove ? 'Remove successed !' : 'Not found !';
     console.log(message);
 } else {
